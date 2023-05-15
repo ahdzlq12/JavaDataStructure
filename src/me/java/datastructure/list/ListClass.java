40bc3a87e2a9e97039688c074ff004e10280aedc
GyuChangSeong <ahdzlq12@gmail.com>
2023-05-16 01:05:26 +0900

    [자료구조] ArrayList add(), get(), remove(), remove()

---
+++ src/me/java/datastructure/list/ListClass.java
@@ -0,0 +1,42 @@
+package me.java.datastructure.list;
+
+import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListClass {
+    private List<String> list = new ArrayList<>();
+
+    public void addData(String data){
+        if(list == null)
+            list = new ArrayList<>();
+
+        list.add(data);
+    }
+
+    public String getData(int index){
+        if(list == null)
+            list = new ArrayList<>();
+
+        if(index < list.size())
+            return list.get(index);
+
+        return null;
+    }
+
+    public void insertData(int index, String data){
+        if(data != null && index < list.size())
+            list.add(index, data);
+    }
+
+    public void removeData(int index){
+        if(index < list.size())
+            list.remove(index);
+    }
+
+
+    public void printData(){
+        list.stream().forEach(n -> System.out.println(n));
+    }
+
+}
