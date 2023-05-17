4188c5991fc690b738a78ff934f9d0b66a754682
GyuChangSeong <ahdzlq12@gmail.com>
2023-05-17 14:49:10 +0900

    [자료구조] LinkedList 구현. size, contains, isEmpty, indexOf, get, deleteByIndex, delete

--- src/me/java/datastructure/linkedlist/LinkedList.java
+++ src/me/java/datastructure/linkedlist/LinkedList.java
@@ -43,22 +43,70 @@ public class LinkedList<T> implements IList<T> {
 
     @Override
     public boolean delete(T t) {
+        Node prev = this.head;
+        Node curr = prev.next;
+
+        while (curr != null) { // prev로 돌아도 될거 같음. 근데 어차피 head는 더미라서 데이터 없음
+            if (curr.data.equals(t)) {
+                prev.next = curr.next;
+                curr.next =null;
+                size--;
+                return true;
+            }
+
+            prev = prev.next;
+            curr = curr.next;
+        }
         return false;
     }
 
     @Override
     public boolean deleteByIndex(int index) {
-        return false;
+        if(index < 0 || index >= this.size)
+            throw new IndexOutOfBoundsException();
+
+        Node prev = this.head;
+        Node curr = prev.next;
+
+        int count = 0;
+        while (count++ < index){
+            prev = prev.next;
+            curr = curr.next;
+        }
+        prev.next = curr.next;
+        curr.next = null;
+        this.size--;
+        return true;
+
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if(index < 0 || index >= this.size)
+            throw new IndexOutOfBoundsException();
+
+        Node curr = this.head.next; //head는 null이기 때문에
+        int count = 0;
+
+        while (count++ < index) {
+            curr = curr.next;
+        }
+
+        return curr.data;
     }
 
     @Override
     public int indexOf(T t) {
-        return 0;
+        Node curr = this.head.next;
+
+        int index = 0;
+        while (curr != null){
+            if(t.equals(curr.data))
+                return index;
+            curr = curr.next;
+            index++;
+        }
+        return -1;
     }
 
     @Override
@@ -69,17 +117,24 @@ public class LinkedList<T> implements IList<T> {
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.head.next == null;
     }
 
     @Override
     public boolean contains(T t) {
+        Node curr = this.head.next;
+        while (curr != null){
+            if(t.equals(curr.data))
+                return true;
+
+            curr = curr.next;
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     private class Node{
